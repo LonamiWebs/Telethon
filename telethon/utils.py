13199f737e84d0b072b04735c2e0023e2292d116
feodoran <feodoran@online.de>
2017-06-16 09:11:49 +0200

    Handle more cases on .get_display_name

--- telethon/utils.py
+++ telethon/utils.py
@@ -15,9 +15,14 @@ def get_display_name(entity):
     """Gets the input peer for the given "entity" (user, chat or channel)
        Returns None if it was not found"""
     if isinstance(entity, User):
-        if entity.last_name is not None:
+        if entity.last_name and entity.first_name:
             return '{} {}'.format(entity.first_name, entity.last_name)
-        return entity.first_name
+        elif entity.first_name:
+            return entity.first_name
+        elif entity.last_name:
+            return entity.last_name
+        else:
+            return '(No name)'
 
     if isinstance(entity, Chat) or isinstance(entity, Channel):
         return entity.title
