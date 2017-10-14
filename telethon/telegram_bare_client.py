d92e8e11add3cc61ddb5fb40676ad9596a786ec5
Lonami Exo <totufals@hotmail.com>
2017-10-14 12:05:28 +0200

    Update to v0.15.2

--- telethon/telegram_bare_client.py
+++ telethon/telegram_bare_client.py
@@ -58,7 +58,7 @@ class TelegramBareClient:
     """
 
     # Current TelegramClient version
-    __version__ = '0.15.1'
+    __version__ = '0.15.2'
 
     # TODO Make this thread-safe, all connections share the same DC
     _dc_options = None
