23989a1c92f7b44337882198fa3b7e618c05fa4b
Andrei Fokau <andrei@5monkeys.se>
2017-10-25 18:56:17 +0200

    Support running tests via setup.py; install PyCrypto for testing.

--- run_tests.py
+++ run_tests.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
+import argparse
+import logging
 import unittest
 
-if __name__ == '__main__':
+logging.basicConfig(level=logging.DEBUG)
+__log__ = logging.getLogger(__name__)
+
+
+def test_suite(skip_network=False):
     from telethon_tests import \
         CryptoTests, ParserTests, TLTests, UtilsTests, NetworkTests
 
     test_classes = [CryptoTests, ParserTests, TLTests, UtilsTests]
 
-    network = input('Run network tests (y/n)?: ').lower() == 'y'
-    if network:
+    if skip_network:
+        __log__.warning("Skipping network tests")
+    else:
+        __log__.info("Running with network tests")
         test_classes.append(NetworkTests)
 
     loader = unittest.TestLoader()
@@ -18,7 +26,15 @@ if __name__ == '__main__':
         suite = loader.loadTestsFromTestCase(test_class)
         suites_list.append(suite)
 
-    big_suite = unittest.TestSuite(suites_list)
+    return unittest.TestSuite(suites_list)
 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n,--skip-network', dest='skip_network',
+                        action='store_true')
+    args = parser.parse_args()
+
+    big_suite = test_suite(skip_network=args.skip_network)
     runner = unittest.TextTestRunner()
     results = runner.run(big_suite)
