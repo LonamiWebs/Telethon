b42b4bb3267bcbce297d79fc2f292f0fc548fb87
Maxim Smirnov <smirnov.cpp@gmail.com>
2017-11-24 20:47:36 +0300

    Add OpenSSL assertion on authenticator.py (#453)
    aes_ige.c(88): OpenSSL internal error, assertion failed

--- telethon/network/authenticator.py
+++ telethon/network/authenticator.py
@@ -113,6 +113,8 @@ def _do_authentication(connection):
     key, iv = utils.generate_key_data_from_nonce(
         res_pq.server_nonce, new_nonce
     )
+    if server_dh_params.encrypted_answer % 16 != 0:
+        raise SecurityError('AES block size missmatch')
     plain_text_answer = AES.decrypt_ige(
         server_dh_params.encrypted_answer, key, iv
     )
