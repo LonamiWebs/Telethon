73edb0f4ff53a9e91f64481ae0d4e529064abebe
Birger Jarl <codefuhrer@gmail.com>
2017-12-27 16:52:33 +0300

    Avoid using None dates on file download (#462)

--- telethon/telegram_client.py
+++ telethon/telegram_client.py
@@ -954,6 +954,8 @@ class TelegramClient(TelegramBareClient):
                 name = None
 
             if not name:
+                if not date:
+                    date = datetime.now()
                 name = '{}_{}-{:02}-{:02}_{:02}-{:02}-{:02}'.format(
                     kind,
                     date.year, date.month, date.day,
