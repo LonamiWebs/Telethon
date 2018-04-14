ace715e0598635830ef9c5a40182ce1a71053d7a
Lonami Exo <totufals@hotmail.com>
2018-04-14 15:02:27 +0200

    Don't log broken packets as individual bytes

--- telethon/network/connection.py
+++ telethon/network/connection.py
@@ -191,10 +191,13 @@ class Connection:
             __log__.error('Read invalid packet length %d, '
                           'reading data left:', packet_len)
             while True:
+                data = b''
                 try:
-                    __log__.error(repr(self.read(1)))
+                    data += self.read(1)
                 except TimeoutError:
                     break
+                finally:
+                    __log__.error(repr(data))
             # Connection reset and hope it's fixed after
             self.conn.close()
             raise ConnectionResetError()
