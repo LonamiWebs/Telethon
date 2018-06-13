898ce34c65fcfa8031422e69c3885da2007240e9
Lonami Exo <totufals@hotmail.com>
2018-06-13 11:41:12 +0200

    Set __all__ on telethon.__init__.py

--- telethon/__init__.py
+++ telethon/__init__.py
@@ -2,8 +2,11 @@ import logging
 from .telegram_bare_client import TelegramBareClient
 from .telegram_client import TelegramClient
 from .network import connection
-from . import tl, version
+from .tl import types, functions
+from . import version, events, utils
 
 
 __version__ = version.__version__
 logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__all__ = ['TelegramClient', 'types', 'functions', 'events', 'utils']
