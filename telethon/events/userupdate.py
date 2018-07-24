7b22c72c3ebd386caa6ce4e71e27a061082a46f9
Lonami Exo <totufals@hotmail.com>
2018-07-24 20:38:38 +0200

    Use UTC timezone for events.UserUpdate

--- telethon/events/userupdate.py
+++ telethon/events/userupdate.py
@@ -96,7 +96,8 @@ class UserUpdate(EventBuilder):
                 isinstance(status, types.UserStatusOnline) else None
 
             if self.last_seen:
-                diff = datetime.datetime.now() - self.last_seen
+                now = datetime.datetime.now(tz=datetime.timezone.utc)
+                diff = now - self.last_seen
                 if diff < datetime.timedelta(days=30):
                     self.within_months = True
                     if diff < datetime.timedelta(days=7):
