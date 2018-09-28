fe45a959dbf6e845cdee68fab02daa376ce2c4a4
joseph <josephgk@hotmail.nl>
2018-09-28 17:34:14 +0200

    asynciowrapper for sessions

--- telethon/network/connection/__init__.py
+++ telethon/network/connection/__init__.py
@@ -3,4 +3,3 @@ from .tcpabridged import ConnectionTcpAbridged
 from .tcpobfuscated import ConnectionTcpObfuscated
 from .tcpintermediate import ConnectionTcpIntermediate
 from .http import ConnectionHttp
-from .common import Connection
