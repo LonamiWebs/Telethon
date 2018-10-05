730cf319212a3d1c9b98bb5dd0986e052b8e8653
Lonami Exo <totufals@hotmail.com>
2018-10-05 19:36:01 +0200

    Delete all (broken) tests

--- telethon_tests/test_tl.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-
-class TLTests(unittest.TestCase):
-    """There are no tests yet"""
-    @unittest.skip("there should be TL tests")
-    def test_tl(self):
-        self.assertTrue(True)
