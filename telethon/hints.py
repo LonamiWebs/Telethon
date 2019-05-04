89c993f567c69c46624dd0fa6e3c9f7f9ea5938c
Lonami Exo <totufals@hotmail.com>
2019-05-04 10:21:21 +0200

    Add missing hints.py file for the previous commit

---
+++ telethon/hints.py
@@ -0,0 +1,60 @@
+import datetime
+import io
+import typing
+
+from . import helpers
+from .tl import types, custom
+
+Phone = str
+Username = str
+PeerID = int
+Entity = typing.Union[types.User, types.Chat, types.Channel]
+FullEntity = typing.Union[types.UserFull, types.messages.ChatFull, types.ChatFull, types.ChannelFull]
+
+EntityLike = typing.Union[
+    Phone,
+    Username,
+    PeerID,
+    types.TypePeer,
+    types.TypeInputPeer,
+    Entity,
+    FullEntity
+]
+EntitiesLike = typing.Union[EntityLike, typing.Sequence[EntityLike]]
+
+ButtonLike = typing.Union[types.TypeKeyboardButton, custom.Button]
+MarkupLike = typing.Union[
+    types.TypeReplyMarkup,
+    ButtonLike,
+    typing.Sequence[ButtonLike],
+    typing.Sequence[typing.Sequence[ButtonLike]]
+]
+
+TotalList = helpers.TotalList
+
+DateLike = typing.Optional[float, datetime.datetime, datetime.date, datetime.timedelta]
+
+
+LocalPath = str
+ExternalUrl = str
+BotFileID = str
+FileLike = typing.Union[
+    LocalPath,
+    ExternalUrl,
+    BotFileID,
+    bytes,
+    io.IOBase,
+    types.TypeMessageMedia,
+    types.TypeInputFile
+]
+
+OutFileLike = typing.Union[
+    str,
+    typing.Type[bytes],
+    io.IOBase
+]
+
+MessageLike = typing.Union[str, types.Message]
+MessageIDLike = typing.Union[int, types.Message, types.TypeInputMessage]
+
+ProgressCallback = typing.Callable[[int, int], None]
