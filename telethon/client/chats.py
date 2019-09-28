88d84244744f9068ff570619132214fca0297fd5
penn5 <penn5@users.noreply.github.com>
2019-09-28 08:38:41 +0100

    Fix kick_participant in channels (#1284)
    Presumably some server-side change made insta-unbanning no longer work.

--- telethon/client/chats.py
+++ telethon/client/chats.py
@@ -1085,18 +1085,17 @@ class ChatMethods:
             if isinstance(user, types.InputPeerSelf):
                 await self(functions.channels.LeaveChannelRequest(entity))
             else:
-                await self([
-                    functions.channels.EditBannedRequest(
-                        channel=entity,
-                        user_id=user,
-                        banned_rights=types.ChatBannedRights(until_date=None, view_messages=True)
-                    ),
-                    functions.channels.EditBannedRequest(
-                        channel=entity,
-                        user_id=user,
-                        banned_rights=types.ChatBannedRights(until_date=None)
-                    ),
-                ], ordered=True)
+                await self(functions.channels.EditBannedRequest(
+                    channel=entity,
+                    user_id=user,
+                    banned_rights=types.ChatBannedRights(until_date=None, view_messages=True)
+                ))
+                await asyncio.sleep(0.5)
+                await self(functions.channels.EditBannedRequest(
+                    channel=entity,
+                    user_id=user,
+                    banned_rights=types.ChatBannedRights(until_date=None)
+                ))
         else:
             raise ValueError('You must pass either a channel or a chat')
 
