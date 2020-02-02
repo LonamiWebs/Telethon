ac8009af4a6f3224375b4f368b041d50a1833b31
Lonami <totufals@hotmail.com>
2020-02-02 10:01:15 +0100

    Fix default DC ID value

--- telethon/client/telegrambaseclient.py
+++ telethon/client/telegrambaseclient.py
@@ -17,7 +17,7 @@ from ..statecache import StateCache
 from ..tl import TLObject, functions, types
 from ..tl.alltlobjects import LAYER
 
-DEFAULT_DC_ID = 4
+DEFAULT_DC_ID = 2
 DEFAULT_IPV4_IP = '149.154.167.51'
 DEFAULT_IPV6_IP = '[2001:67c:4e8:f002::a]'
 DEFAULT_PORT = 443
