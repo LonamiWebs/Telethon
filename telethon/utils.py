55e6c764265c29d72aef0675dd07ef6906eb3978
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2020-09-21 14:23:31 +0530

    add replies to the list
    https://t.me/tginfoen/953

--- telethon/utils.py
+++ telethon/utils.py
@@ -67,7 +67,7 @@ TG_JOIN_RE = re.compile(
 # See https://telegram.org/blog/inline-bots#how-does-it-work
 VALID_USERNAME_RE = re.compile(
     r'^([a-z](?:(?!__)\w){3,30}[a-z\d]'
-    r'|gif|vid|pic|bing|wiki|imdb|bold|vote|like|coub)$',
+    r'|gif|vid|pic|bing|wiki|imdb|bold|vote|like|coub|replies)$',
     re.IGNORECASE
 )
 
