5f42bb66bb908fcc21c2c24bc87476e28e33e1f6
luckydonald <mac2019.git@luckydonald.de>
2020-11-08 21:13:09 +0100

    Clean away that comment.

--- setup.py
+++ setup.py
@@ -233,9 +233,6 @@ def main(argv):
                 'telethon_*', 'tests*'
             ]),
 
-            # List run-time dependencies here. These will be installed by pip when your
-            # project is installed. For an analysis of "install_requires" vs pip's
-            # requirements files see:
             # https://packaging.python.org/en/latest/requirements.html
             install_requires=[
                 'pyaes', 'rsa',
