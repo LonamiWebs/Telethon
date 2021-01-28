4e73577d595545be8c2292329eb9d50fc3488511
Lonami Exo <totufals@hotmail.com>
2021-01-28 19:48:01 +0100

    Update to layer 123

--- telethon/client/dialogs.py
+++ telethon/client/dialogs.py
@@ -456,7 +456,8 @@ class DialogMethods:
         if ty == helpers._EntityType.CHAT and not deactivated:
             try:
                 result = await self(functions.messages.DeleteChatUserRequest(
-                    entity.chat_id, types.InputUserSelf()))
+                    entity.chat_id, types.InputUserSelf(), revoke=revoke
+                ))
             except errors.PeerIdInvalidError:
                 # Happens if we didn't have the deactivated information
                 result = None
