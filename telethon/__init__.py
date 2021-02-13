0e8bd8248cc649637b7c392616887c50986427a0
Lonami Exo <totufals@hotmail.com>
2021-02-14 00:25:54 +0100

    Fix patched module was never automatically imported
    Closes #1701. It has to be imported late in the process of
    `import telethon` for its side-effects.

--- telethon/__init__.py
+++ telethon/__init__.py
@@ -2,6 +2,7 @@ from .client.telegramclient import TelegramClient
 from .network import connection
 from .tl import types, functions, custom
 from .tl.custom import Button
+from .tl import patched as _  # import for its side-effects
 from . import version, events, utils, errors
 
 __version__ = version.__version__
