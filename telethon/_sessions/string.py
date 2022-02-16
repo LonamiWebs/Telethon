4fae119a18b8e5c55be6074e05fa4c8941f6ba89
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2022-02-16 20:29:56 +0530

    Fix StringSession usage of IPv6 (#3726)

--- telethon/_sessions/string.py
+++ telethon/_sessions/string.py
@@ -74,15 +74,15 @@ class StringSession(MemorySession):
         if not self.state:
             return ''
 
-        if self.state.ipv6 is not None:
-            ip = self.state.ipv6.to_bytes(16, 'big', signed=False)
+        if self.dcs[self.state.dc_id].ipv6 is not None:
+            ip = self.dcs[self.state.dc_id].ipv6.to_bytes(16, 'big', signed=False)
         else:
-            ip = self.state.ipv6.to_bytes(4, 'big', signed=False)
+            ip = self.dcs[self.state.dc_id].ipv6.to_bytes(4, 'big', signed=False)
 
         return CURRENT_VERSION + StringSession.encode(struct.pack(
             _STRUCT_PREFORMAT.format(len(ip)),
             self.state.dc_id,
             ip,
-            self.state.port,
+            self.dcs[self.state.dc_id].port,
             self.dcs[self.state.dc_id].auth
         ))
