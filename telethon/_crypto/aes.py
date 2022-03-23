c6d3341cd51cadb3a025a83ea293b15fbbb51d70
BlackCatDevel0per <bcdev@mail.ru>
2022-03-23 20:28:44 +0500

    Rolled back

--- telethon/_crypto/aes.py
+++ telethon/_crypto/aes.py
@@ -2,7 +2,6 @@
 AES IGE implementation in Python.
 
 If available, cryptg will be used instead, otherwise
-If available, pyaesni(intel aes-ni for python) will be used instead, otherwise
 if available, libssl will be used instead, otherwise
 the Python implementation will be used.
 """
@@ -13,35 +12,19 @@ from . import libssl
 
 
 __log__ = logging.getLogger(__name__)
-# tgcrypto
-try:
-    import tgcrypto
-    __log__.info('pyaesni detected, it will be used for encryption')
-except ImportError:
-    tgcrypto = None
-    __log__.info('tgcrypto module not installed and libssl not found, '
-                 'falling back to (slower) Python encryption')
-# pyaesni (intel aes-ni)
-try:
-    if not tgcrypto:
-        import pyaesni
-        __log__.info('pyaesni detected, it will be used for encryption')
-except ImportError:
-    pyaesni = None
-    __log__.info('pyaesni module not installed and libssl not found, '
-                 'falling back to (slower) Python encryption')
+
 
 try:
-    if not tgcrypto and not pyaesni:
-      import cryptg
-      __log__.info('cryptg detected, it will be used for encryption')
+    import cryptg
+    __log__.info('cryptg detected, it will be used for encryption')
 except ImportError:
     cryptg = None
-    __log__.info('cryptg module not installed and libssl not found, '
-                 'falling back to (slower) Python encryption')
-if not tgcrypto and not pyaesni and not cryptg:
     if libssl.encrypt_ige and libssl.decrypt_ige:
         __log__.info('libssl detected, it will be used for encryption')
+    else:
+        __log__.info('cryptg module not installed and libssl not found, '
+                     'falling back to (slower) Python encryption')
+
 
 class AES:
     """
@@ -54,12 +37,8 @@ class AES:
         Decrypts the given text in 16-bytes blocks by using the
         given key and 32-bytes initialization vector.
         """
-        if tgcrypto:
-            return tgcrypto.ige256_decrypt(cipher_text, key, iv)
-        elif cryptg:
+        if cryptg:
             return cryptg.decrypt_ige(cipher_text, key, iv)
-        elif pyaesni:
-            return pyaesni.ige256_decrypt(cipher_text, key, iv)
         if libssl.decrypt_ige:
             return libssl.decrypt_ige(cipher_text, key, iv)
 
@@ -99,12 +78,8 @@ class AES:
         if padding:
             plain_text += os.urandom(16 - padding)
 
-        if tgcrypto:
-            return tgcrypto.ige256_encrypt(plain_text, key, iv)
-        elif cryptg:
+        if cryptg:
             return cryptg.encrypt_ige(plain_text, key, iv)
-        elif pyaesni:
-            return pyaesni.ige256_encrypt(plain_text, key, iv)
         if libssl.encrypt_ige:
             return libssl.encrypt_ige(plain_text, key, iv)
 
