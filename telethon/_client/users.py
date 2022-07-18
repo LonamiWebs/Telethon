7d0f63798c2a1b71f3c237b6c833dc26b1a32890
unclamped <clip1809@protonmail.com>
2022-07-18 18:54:07 -0300

    Fix error message

--- telethon/_client/users.py
+++ telethon/_client/users.py
@@ -283,7 +283,7 @@ async def _get_input_peer(
 
     raise ValueError(
         'Could not find the input peer for {} ({}). Please read https://'
-        'docs.telethon.dev/en/latest/concepts/entities.html to'
+        'docs.telethon.dev/en/stable/concepts/entities.html to'
         ' find out more details.'
         .format(peer, type(peer).__name__)
     )
