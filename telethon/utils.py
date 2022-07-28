362d06654f52c2f8b1dfc121be0f80b346daf22f
Devesh Pal <newdev0@outlook.com>
2022-07-28 16:00:46 +0530

    Support sending 4GB files (#3891)

--- telethon/utils.py
+++ telethon/utils.py
@@ -1341,10 +1341,7 @@ def get_appropriated_part_size(file_size):
         return 128
     if file_size <= 786432000:  # 750MB
         return 256
-    if file_size <= 2097152000:  # 2000MB
-        return 512
-
-    raise ValueError('File size too large')
+    return 512
 
 
 def encode_waveform(waveform):
