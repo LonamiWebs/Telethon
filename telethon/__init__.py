7cac3668d6ad99db7fe08d23a023535edd2429cb
Lonami Exo <totufals@hotmail.com>
2022-09-20 12:59:36 +0200

    Make custom, functions and types proper modules
    This allows "from telethon.types import Message" to work.
    Closes #3929.
    
    Not entirely sure how it used to work before, perhaps
    it got changed at some point but this should revert previous
    behaviour.

--- telethon/__init__.py
+++ telethon/__init__.py
@@ -1,9 +1,8 @@
 from .client.telegramclient import TelegramClient
 from .network import connection
-from .tl import types, functions, custom
 from .tl.custom import Button
 from .tl import patched as _  # import for its side-effects
-from . import version, events, utils, errors
+from . import version, events, utils, errors, types, functions, custom
 
 __version__ = version.__version__
 
---
+++ telethon/custom.py
@@ -0,0 +1,1 @@
+from .tl.custom import *
