LonamiWebs/Telethon

commit b6d8311a5565a84986a3bd07d9539c55d9b7ca4e

Fix iter_messages was stopping too early in some channels
Closes #3949.

telethon/client/messages.py
@@ -204,7 +204,20 @@ class _MessagesIter(RequestIter):
             message._finish_init(self.client, entities, self.entity)
             self.buffer.append(message)
 
-        if len(r.messages) < self.request.limit:
+        # Some channels are "buggy" and may return less messages than
+        # requested (apparently, the messages excluded are, for example,
+        # "not displayable due to local laws").
+        #
+        # This means it's not safe to rely on `len(r.messages) < req.limit` as
+        # the stop condition. Unfortunately more requests must be made.
+        #
+        # However we can still check if the highest ID is equal to or lower
+        # than the limit, in which case there won't be any more messages
+        # because the lowest message ID is 1.
+        #
+        # We also assume the API will always return, at least, one message if
+        # there is more to fetch.
+        if not r.messages or r.messages[0].id <= self.request.limit:
             return True
 
         # Get the last message that's not empty (in some rare cases

commit b3266fabd8d4d2a6bfd194b5d81201cf28f48a55

Fix iter_messages could get stuck on global search
Found thanks to #920. Issue probably introduced in b6d8311.

telethon/client/messages.py
@@ -204,6 +204,10 @@ class _MessagesIter(RequestIter):
             message._finish_init(self.client, entities, self.entity)
             self.buffer.append(message)
 
+        # Not a slice (using offset would return the same, with e.g. SearchGlobal).
+        if isinstance(r, types.messages.Messages):
+            return True
+
         # Some channels are "buggy" and may return less messages than
         # requested (apparently, the messages excluded are, for example,
         # "not displayable due to local laws").