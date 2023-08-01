6ded164b85e32ddd1e72e847a91a01155f796fcd
Bernhard M. Wiedemann <githubbmwprimary@lsmod.de>
2023-08-01 20:23:24 +0200

    Sort tlobjects before generating their listing (#4163)

--- telethon_generator/generators/tlobject.py
+++ telethon_generator/generators/tlobject.py
@@ -667,6 +667,7 @@ def _write_all_tlobjects(tlobjects, layer, builder):
     builder.current_indent += 1
 
     # Fill the dictionary (0x1a2b3c4f: tl.full.type.path.Class)
+    tlobjects.sort(key=lambda x: x.name)
     for tlobject in tlobjects:
         builder.write('{:#010x}: ', tlobject.id)
         builder.write('functions' if tlobject.is_function else 'types')
