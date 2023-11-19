daa64b55f24f2ad610995bb82bd1e0ed74538320
Mario-ttide <mario@mariottide.eu>
2023-11-19 17:01:24 +0100

    Retry also on TimedOutError

--- telethon/client/users.py
+++ telethon/client/users.py
@@ -89,6 +89,7 @@ class UserMethods:
                     return result
             except (errors.ServerError, errors.RpcCallFailError,
                     errors.RpcMcgetFailError, errors.InterdcCallErrorError,
+                    errors.TimedOutError,
                     errors.InterdcCallRichErrorError) as e:
                 last_error = e
                 self._log[__name__].warning(
