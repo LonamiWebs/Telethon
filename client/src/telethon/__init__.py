837258f4c826b7f37153b6a361887504418d1dfe
Lonami Exo <totufals@hotmail.com>
2024-03-16 19:51:51 +0100

    Export __version__ from root

--- client/src/telethon/__init__.py
+++ client/src/telethon/__init__.py
@@ -1,9 +1,11 @@
 """
 The main package for the Telethon library.
 """
+
 from ._impl import tl as _tl
 from ._impl.client import Client
 from ._impl.client.errors import errors
 from ._impl.mtproto import RpcError
+from .version import __version__
 
-__all__ = ["_tl", "Client", "errors", "RpcError"]
+__all__ = ["_tl", "Client", "errors", "RpcError", "__version__"]
