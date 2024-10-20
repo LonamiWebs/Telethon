3f589b287deca7b3aa9abc5136bec2ec26a4e7ae
Arkadii Halchenko <106999186+EntireMusic@users.noreply.github.com>
2024-10-20 20:50:08 +0300

    Fix parse_mode with albums (#4494)
    Closes #4492.

--- telethon/client/uploads.py
+++ telethon/client/uploads.py
@@ -422,7 +422,7 @@ class UploadMethods:
 
             return result
 
-        if formatting_entities is not None:
+        if formatting_entities:
             msg_entities = formatting_entities
         else:
             caption, msg_entities =\
