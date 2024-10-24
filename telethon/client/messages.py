3a1d25c80a7c7c56bb54e11adfacac983269164b
Gregory Eremeev <gregory.eremeev@gmail.com>
2024-10-24 15:53:25 -0300

    Added caption extraction when send a msg with a file (#4499)
    Closes #4499

--- telethon/client/messages.py
+++ telethon/client/messages.py
@@ -826,6 +826,8 @@ class MessageMethods:
                 await client.send_message(chat, 'Hi, future!', schedule=timedelta(minutes=5))
         """
         if file is not None:
+            if isinstance(message, types.Message):
+                message = message.message
             return await self.send_file(
                 entity, file, caption=message, reply_to=reply_to,
                 attributes=attributes, parse_mode=parse_mode,
