ba393bdbe23e8b7f6d4fc28542cfb5d6c7f1cc91
abcdenis <abcdenis@users.noreply.github.com>
2025-03-14 17:17:44 +0300

    skip ChannelParticipantLeft in _ParticipantsIter

--- telethon/client/chats.py
+++ telethon/client/chats.py
@@ -218,8 +218,10 @@ class _ParticipantsIter(RequestIter):
         self.requests.offset += len(participants.participants)
         users = {user.id: user for user in participants.users}
         for participant in participants.participants:
-
-            if isinstance(participant, types.ChannelParticipantBanned):
+            if isinstance(participant, types.ChannelParticipantLeft):
+                # See issue #3231 to learn why this is ignored.
+                continue
+            elif isinstance(participant, types.ChannelParticipantBanned):
                 if not isinstance(participant.peer, types.PeerUser):
                     # May have the entire channel banned. See #3105.
                     continue
