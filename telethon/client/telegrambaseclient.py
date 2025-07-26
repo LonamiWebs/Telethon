41c74f50a76dd011076622c53a5daa8b1363154c
humbertogontijo <96875138+humbertogontijo@users.noreply.github.com>
2025-07-26 10:30:18 -0300

    Fix typo

--- telethon/client/telegrambaseclient.py
+++ telethon/client/telegrambaseclient.py
@@ -321,7 +321,7 @@ class TelegramBaseClient(abc.ABC):
         self.api_hash = api_hash
 
         if inspect.iscoroutinefunction(self.session.process_entities):
-            self._log[__name__].warning('Using async sessions support is a experimental feature')
+            self._log[__name__].warning('Using async sessions support is an experimental feature')
 
         # Current proxy implementation requires `sock_connect`, and some
         # event loops lack this method. If the current loop is missing it,
