274dd209eb102f44fce11a6771fc617f1446d27b
habcawa <212562656+habcawa@users.noreply.github.com>
2025-11-07 11:24:42 +0100

    fix: missing error handling for AUTH_KEY_PERM_EMPTY as pointed out by andrew-ld

--- telethon/client/messages.py
+++ telethon/client/messages.py
@@ -923,7 +923,13 @@ class MessageMethods:
                 effect=message_effect_id
             )
 
-        result = await self(request)
+        try:
+            result = await self(request)
+
+        except errors.rpcerrorlist.AuthKeyPermEmptyError as e:
+            await self._sender._reconnect(e)
+            result = await self(request)
+
         if isinstance(result, types.UpdateShortSentMessage):
             message = types.Message(
                 id=result.id,
